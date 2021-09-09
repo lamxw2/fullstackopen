eecd8d76d964206e618c334dc3ea7952e612f310
lamxw2 <lamxw3@gmail.com>
2021-09-09 20:08:09 +0100

    Added app.js to unicafe

---
+++ part1/unicafe/App.js
@@ -0,0 +1,95 @@
+import React, { useState } from 'react'
+
+const Header = ({name}) => {
+  return (
+    <h1>
+      {name}
+    </h1>
+  )
+}
+
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const StatisticLine = ({text, count}) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{count}</td>
+    </tr>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const textGood = 'good'
+  const textNeutral = 'neutral'
+  const textBad = 'bad'
+
+  const calcTotal = (counts) => {
+    return (
+      counts[0] + counts[1] + counts[2]
+    )
+  }
+
+  const average = (total) => {
+    return (
+      total/3
+    )
+  }
+
+  const positive = (good, total) => {
+    return (
+        good/total*100 + ' %'
+      )
+  }
+
+  let total = calcTotal([good, neutral, bad])
+
+  const display = () => {
+    if (total === 0) {
+      return <p>No feedback given</p>
+    }
+    else {
+      return (
+        <table>
+          <tbody>
+            <StatisticLine text={textGood} count={good} />
+            <StatisticLine text={textNeutral} count={neutral} />
+            <StatisticLine text={textBad} count={bad} />
+
+            <StatisticLine text='all' count={total} />
+            <StatisticLine text='average' count={average(total)} />
+            <StatisticLine text='positive' count={positive(good, total)} />
+          </tbody>
+        </table>
+      )
+    }
+  }
+
+  return (
+    <div>
+      <div>
+        <Header name='give feedback' />
+        <Button handleClick={() => setGood(good + 1)} text={textGood} />
+        <Button handleClick={() => setNeutral(neutral + 1)} text={textNeutral} />
+        <Button handleClick={() => setBad(bad + 1)} text={textBad} />
+      </div>
+      <div>
+        <Header name='statistics' />
+
+        {display()}
+
+      </div>
+    </div>
+  )
+}
+
+export default App
